90868a30e79e9c707b62afeb6fb228fa4953508f
shinkawa <slinkmagic@gmail.com>
2017-11-18 14:02:02 +0900

    a

--- ygo3/src/main/java/com/app/controller/SampleController.java
+++ ygo3/src/main/java/com/app/controller/SampleController.java
@@ -4,15 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.common.Constant;
-
 @Controller
 public class SampleController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String show() {
     	
-    	
-        return Constant.RequestKey.LOGIN.getKey();
+
+    	return "test";
+//        return Constant.RequestKey.LOGIN.getKey();
     }
 }
--- ygo3/src/main/java/com/common/Constant.java
+++ ygo3/src/main/java/com/common/Constant.java
@@ -6,7 +6,7 @@ public class Constant {
 	}
 
 	public static enum RequestKey {
-		LOGIN("LOGIN"),
+		LOGIN("login"),
 		;
 	    private final String key;
 	    
